67e2d08ff54b87f8c840b2d1715b943cb22145cc
William Funk <wfunk@knights.ucf.edu>
2016-11-23 09:51:50 -0500

    Now the widget's self-build command can work from either in Devmateria or without.

--- gulpfile.js
+++ gulpfile.js
@@ -19,7 +19,11 @@ var sass = require('gulp-sass');
 var uglify = require('gulp-uglify');
 var zip = require('gulp-zip');
 
-var configs = require('../../backend/config.json');
+var configs = null;
+if (fs.existsSync('../../backend/config.json'))
+{
+	configs = require('../../backend/config.json');
+}
 
 var widget = sanitize("this-or-that");
 // When compiling this may be entered as an argument.
