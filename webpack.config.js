25daef0ef78e9d2ff189a8a20ff196e73306e195
Ian Turgeon <iturgeon@gmail.com>
2017-07-30 23:02:42 -0400

    updated widget-dev-kit

--- webpack.config.js
+++ webpack.config.js
@@ -1,2 +1,2 @@
 // load the reusable legacy webpack config from materia-widget-dev
-module.exports = require('materia-widget-dev/webpack-widget').getLegacyWidgetBuildConfig()
+module.exports = require('materia-widget-development-kit/webpack-widget').getLegacyWidgetBuildConfig()
