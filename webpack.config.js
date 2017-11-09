212d3d683dd200f8420036fa988f479601dfcd42
Corey Peterson <corey.peterson@ucf.edu>
2017-11-09 10:42:11 -0500

    Updated widget dev kit dependency

--- webpack.config.js
+++ webpack.config.js
@@ -1,2 +1,6 @@
+const path = require('path')
+
 // load the reusable legacy webpack config from materia-widget-dev
-module.exports = require('materia-widget-development-kit/webpack-widget').getLegacyWidgetBuildConfig()
+let webpackConfig = require('materia-widget-development-kit/webpack-widget').getLegacyWidgetBuildConfig()
+
+module.exports = webpackConfig
